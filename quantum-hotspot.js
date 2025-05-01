5c0c0c65d6a8097726bc37d8370f80bc96bbdf42
xingxerx <xingxerx@gmail.com>
2025-04-30 21:07:52 -0400

    Updated file: quantum-hotspot.js

---
+++ quantum-hotspot.js
@@ -0,0 +1,10 @@
+App.js
+  index.js
+ server/ (Node JS backend)
+  models/ (MongoDB schemas)
+  routes/ (API endpoints)
+  utils/ (quantum integration and security)
+  app.js
+ quantum/ (Qiskit integration)
+  quantum.js
+ package.json
